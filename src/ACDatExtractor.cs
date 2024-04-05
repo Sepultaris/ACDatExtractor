068b58b177ca234c3bebe546702b356b0a11e9a3
Sepultaris <49662778+Sepultaris@users.noreply.github.com>
2024-04-05 19:28:13 -0400

    Created an explicit main

---
+++ src/ACDatExtractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACDatExtractor
+{
+    public class ACDatExtractor
+    {
+        public static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+        }
+    }
+}
